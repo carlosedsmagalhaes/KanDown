b8f40cc4760428d19d7a24e305e239c7916687dc
Carlos Magalhães <carlosedsmagalhaes@gmail.com>
2025-07-22 14:49:25 -0300

    fix(Ajusta métodos de delete e alteração da posição das colunas): - Reorganiza as posições após um delete ou put;
    feat(Adiciona dicionário de dados e DER)

--- apps/api/routes/colunaRoutes.ts
+++ apps/api/routes/colunaRoutes.ts
@@ -5,7 +5,7 @@ const routes = Router();
 const colunaController = new ColunaController();
 
 routes.get("/:quadro_id", colunaController.getColunasByQuadro);
-routes.put("/:id/posicao", colunaController.putColunaPosicao);
+routes.put("/:id/posicao/nova_posicao", colunaController.putColunaPosicao);
 routes.post("/", colunaController.createColuna);
 routes.delete("/:id", colunaController.deleteColuna);
 
